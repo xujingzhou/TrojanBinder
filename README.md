445b6b80d4f37c2c83fa00cfb5e2236f7cfbfce5
Johnny Xu <xujingzhou@users.noreply.github.com>
2017-06-23 11:01:09 +0800

    commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # TrojanBinder
+
+  Trojan file binder. Compiled by VC6/VS2010 and Tested on Win2000/Win10.
+
+  文件捆绑器可用来将二个不同的可执行文件合并成一个文件，运行合并后的文件等同于同时运行合并前的二个文件，并提供同步和异步运行二种方式，操作十分简便。
+
+注意：
+  如果文件编释后不能运行的原因可能是：1、请把它置为RELEASE版再编释。2、可能是编释器设置不同，使最终生成文件大小不同，请把您RELEASE版编释后的文件大小记下，在CBindFileDlg类中的初始对话框类OnInitDialog中的最后，把原来的文件大小184K，替换后您新编释后的文件大小既可。
+
+徐景周(Johnny Xu)
+未来工作室(Future Studio)
---
+++ Resource.h
@@ -0,0 +1,38 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by BindFile.rc
+//
+#define IDC_BROWSE                      4
+#define IDC_FIRSTBROWSE                 4
+#define IDC_BROWSE2                     5
+#define IDC_FINALBROWSE                 5
+#define IDC_SECONDBROWSE                6
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_BINDFILE_DIALOG             102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     129
+#define IDB_BITMAP2                     130
+#define IDC_BUTTON_ABOUT                1000
+#define IDC_BUTTON_BINDFILE             1001
+#define IDC_RADIO_SYNC                  1002
+#define IDC_RADIO_ASYNC                 1003
+#define IDC_PATH                        1012
+#define IDC_FIRSTPATH                   1012
+#define IDC_PATH2                       1013
+#define IDC_FINALPATH                   1013
+#define IDC_SECONDPATH                  1014
+#define IDC_PROGRESS                    1034
+#define IDC_PARTS                       1035
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
